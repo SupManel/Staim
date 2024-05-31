31f8b1955e3e7f2e5c223c6e4a2c9194513fdb8f
SupManel <emanuelramospaiva@gmail.com>
2024-05-31 12:36:58 -0300

    Começando o header e o nav

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Staim</title>
+    <meta name="description" content="Site Staim de Campo Mourão">
+    <meta name="keywords" content="steam, staim, jogos, site de jogos">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="shortcut icon" href="Imagens/T-removebg-preview.png">
+</head>
+<body>
+    <header class="header">
+        <a href="index.html" title="Staim" class="header-logo">
+            <img src="Imagens/T2.png">
+        </a>
+        <nav class="header-menu">
+            <ul>
+                <li>
+                    <a href="">
+                        
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
