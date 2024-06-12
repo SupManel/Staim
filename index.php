75c4434a699288b612509a69fb12df17ca34bb60
SupManel <emanuelramospaiva@gmail.com>
2024-06-12 16:39:50 -0300

    Adicionando o navbar, carrosel e os cards com bootstrap

--- index.php
+++ index.php
@@ -6,33 +6,109 @@
     <title>Staim</title>
     <meta name="description" content="Site Staim de Campo Mourão">
     <meta name="keywords" content="steam, staim, jogos, site de jogos">
+    <script src="bootstrap-5.3.3/dist/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap.css">
     <link rel="stylesheet" href="CSS/style.css">
     <link rel="shortcut icon" href="Imagens/T-removebg-preview.png">
 </head>
 <body>
-    <header class="header">
-        <a href="index.php" title="Staim" class="header-logo">
-            <img src="Imagens/T2.png">
-        </a>
-        <nav class="header-menu">
-            <ul>
-                <li>
-                    <a href="">
-                        a
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        a
-                    </a>
-                </li>
-                <li>
-                    <a href="">
-                        a
-                    </a>
-                </li>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="Imagens/T-removebg-preview.png" alt="Staim">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Dropdown
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><hr class="dropdown-divider"></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
             </ul>
-        </nav>
-    </header>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+  <div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="Imagens/unnamed.jpg" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+      </div>
+      <div class="carousel-item">
+        <img src="Imagens/1e789149967aa9ecee20ef58911a9d17.jpg" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+      </div>
+      <div class="carousel-item">
+        <img src="Imagens/beautiful_night_city-wallpaper-1920x600.jpg" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <div class="row row-cols-1 row-cols-md-3 g-4">
+    <div class="col">
+      <div class="card h-100">
+        <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a short card.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <div class="card h-100">
+        <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </body>
 </html>
