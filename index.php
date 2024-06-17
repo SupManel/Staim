88dfe6ac16c845214406c577956a389812b4c6ed
SupManel <emanuelramospaiva@gmail.com>
2024-06-17 20:47:18 -0300

    Continuando de fazer os cards, paginas dos outros jogos ainda por adicionar

--- index.php
+++ index.php
@@ -1,53 +1,52 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Staim</title>
-    <meta name="description" content="Site Staim de Campo Mourão">
-    <meta name="keywords" content="steam, staim, jogos, site de jogos">
-    <script src="bootstrap-5.3.3/dist/js/bootstrap.bundle.js"></script>
-    <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap-grid.css">
-    <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="CSS/style.css">
-    <link rel="shortcut icon" href="Imagens/T-removebg-preview.png">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Staim</title>
+  <meta name="description" content="Site Staim de Campo Mourão">
+  <meta name="keywords" content="steam, staim, jogos, site de jogos">
+  <script src="bootstrap-5.3.3/dist/js/bootstrap.bundle.js"></script>
+  <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap-grid.css">
+  <link rel="stylesheet" href="bootstrap-5.3.3/dist/css/bootstrap.css">
+  <link rel="stylesheet" href="CSS/style.css">
+  <link rel="shortcut icon" href="Imagens/T-removebg-preview.png">
 </head>
+
 <body>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-        <img src="Imagens/T-removebg-preview.png" alt="Staim">
+      <a class="navbar-brand" href="index.php">
+        <img src="Imagens/T-removebg-preview.png" alt="Staim" title="Staim" href="index.php">
       </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="col-12 col-md-4lapse"
+        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+        aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <div class="col-12 col-md-4lapse navbar-col-12 col-md-4lapse" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Fale Conosco</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
+              Games
             </a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#">BadBoy in a Dreamland</a></li>
+              <li><a class="dropdown-item" href="#">Jogo</a></li>
+              <li><a class="dropdown-item" href="#">Jogo</a></li>
+              <li><a class="dropdown-item" href="#">Jogo</a></li>
+              <li><a class="dropdown-item" href="#">Jogo</a></li>
+              <li><a class="dropdown-item" href="#">Jogo</a></li>
             </ul>
           </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
       </div>
     </div>
   </nav>
@@ -57,10 +56,12 @@
         <img src="Imagens/unnamed.jpg" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
       </div>
       <div class="carousel-item">
-        <img src="Imagens/1e789149967aa9ecee20ef58911a9d17.jpg" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+        <img src="Imagens/1e789149967aa9ecee20ef58911a9d17.jpg" class="d-block w-100"
+          alt="Imagens/Badboy in a wasteland.png">
       </div>
       <div class="carousel-item">
-        <img src="Imagens/beautiful_night_city-wallpaper-1920x600.jpg" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+        <img src="Imagens/beautiful_night_city-wallpaper-1920x600.jpg" class="d-block w-100"
+          alt="Imagens/Badboy in a wasteland.png">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -72,43 +73,83 @@
       <span class="visually-hidden">Next</span>
     </button>
   </div>
-  <div class="row row-cols-1 row-cols-md-3 g-4">
-    <div class="col">
-      <div class="card h-100">
-        <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+  <div class="container">
+    <h1 class="text-center">Melhores Games</h1>
+    <div class="row">
+      <div class="col-12 col-md-4">
+        <div class="card text-center mb-3">
+          <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="BadBoy in a Dreamland">
+          <div class="card-body">
+            <div class="text-center">
+              <h5 class="card-title">BadBoy in a Dreamland</h5>
+              <a href="Paginas/badboy.php" class="btn btn-info">Info</a>
+              <a href="Paginas/jbadboy.php" class="btn btn-success">Jogar</a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card h-100">
-        <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a short card.</p>
+      <div class="col-12 col-md-4">
+        <div class="card text-center mb-3">
+          <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+          <div class="card-body">
+            <h5 class="card-title">Jogo2</h5>
+            <a href="Paginas/jogo2.php" class="btn btn-info">Info</a>
+            <a href="Paginas/jjogo2.php" class="btn btn-success">Jogar</a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card h-100">
-        <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      <div class="col-12 col-md-4">
+        <div class="card text-center mb-3">
+          <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+          <div class="card-body">
+            <h5 class="card-title">Jogo3</h5>
+            <a href="Paginas/jogo3.php" class="btn btn-info">Info</a>
+            <a href="Paginas/jjogo.php" class="btn btn-success">Jogar</a>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="col">
-      <div class="card h-100">
-        <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <div class="col-12 col-md-4">
+        <div class="card text-center mb-3">
+          <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+          <div class="card-body">
+            <h5 class="card-title">Jogo4</h5>
+            <a href="Paginas/jogo4.php" class="btn btn-info">Info</a>
+            <a href="Paginas/jjogo4.php" class="btn btn-success">Jogar</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <div class="card text-center mb-3">
+          <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+          <div class="card-body">
+            <h5 class="card-title">Jogo5</h5>
+            <a href="Paginas/jogo5.php" class="btn btn-info">Info</a>
+            <a href="Paginas/jjogo5.php" class="btn btn-success">Jogar</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <div class="card text-center mb-3">
+          <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+          <div class="card-body">
+            <h5 class="card-title">Jogo6</h5>
+            <a href="Paginas/jogo6.php" class="btn btn-info">Info</a>
+            <a href="Paginas/jjogo6.php" class="btn btn-success">Jogar</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-12 col-md-4">
+        <div class="card text-center mb-3">
+          <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
+          <div class="card-body">
+            <h5 class="card-title">Jogo7</h5>
+            <a href="Paginas/jogo7.php" class="btn btn-info">Info</a>
+            <a href="Paginas/jjogo7.php" class="btn btn-success">Jogar</a>
+          </div>
         </div>
       </div>
     </div>
   </div>
 </body>
+
 </html>
