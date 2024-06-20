d9c0c4646a6f7d5c8da0c852ae13516380ccbe79
SupManel <emanuelramospaiva@gmail.com>
2024-06-19 22:59:51 -0300

    Deixando todos links responsivos e fazendo o navbar funcionar no mobile

--- index.php
+++ index.php
@@ -15,57 +15,50 @@
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg bg-secondary">
+  <nav class="navbar navbar-expand-lg bg-secondary text-uppercase">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.php">
-        <img src="Imagens/T-removebg-preview.png" alt="Staim" title="Staim" href="index.php" id="logo">
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="col-12 col-md-4lapse"
-        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-        aria-label="Toggle navigation">
+      <a class="navbar-brand" href="index.php"><img src="Imagens/T-removebg-preview.png"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="col-12 col-md-4lapse navbar-col-12 col-md-4lapse" id="navbarSupportedContent">
-        <div class="text-uppercase">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Fale Conosco</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                aria-expanded="false">
-                Games
-              </a>
-              <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#">BadBoy in a Dreamland</a></li>
-                <li><a class="dropdown-item" href="#">Jogo</a></li>
-                <li><a class="dropdown-item" href="#">Jogo</a></li>
-                <li><a class="dropdown-item" href="#">Jogo</a></li>
-                <li><a class="dropdown-item" href="#">Jogo</a></li>
-                <li><a class="dropdown-item" href="#">Jogo</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link active" href="faleconosco.php">Fale Conosco</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Games
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="badboy.php">BadBoy in a Dreamland</a></li>
+              <li><a class="dropdown-item" href="jogo2.php">Jogo2</a></li>
+              <li><a class="dropdown-item" href="jogo3.php">Jogo3</a></li>
+              <li><a class="dropdown-item" href="jogo4.php">Jogo4</a></li>
+              <li><a class="dropdown-item" href="jogo5.php">Jogo5</a></li>
+              <li><a class="dropdown-item" href="jogo6.php">Jogo6</a></li>
+            </ul>
+          </li>
       </div>
     </div>
   </nav>
   <div id="carouselExample" class="carousel slide">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="Imagens/Destiny.webp" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+        <img src="Imagens/Destiny.webp" class="d-block w-100" alt="Jogo">
       </div>
       <div class="carousel-item">
-        <img src="Imagens/Carro.jpg" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+        <img src="Imagens/Carro.jpg" class="d-block w-100" alt="Jogo">
       </div>
       <div class="carousel-item">
-        <img src="Imagens/Iki.webp" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+        <img src="Imagens/Iki.webp" class="d-block w-100" alt="Jogo">
       </div>
       <div class="carousel-item">
-        <img src="Imagens/Logojogo2.jpg" class="d-block w-100" alt="Imagens/Badboy in a wasteland.png">
+        <img src="Imagens/Logojogo2.jpg" class="d-block w-100" alt="Jogo">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -83,7 +76,7 @@
       <div class="col-12 col-md-4">
         <div class="card text-center mb-3" data-aos="fade-down-right">
           <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="BadBoy in a Dreamland" id="image-card">
-          <div class="card-body bg-secondary">
+          <div class="card-body">
             <div class="text-center">
               <h5 class="card-title text-uppercase">BadBoy in a Dreamland</h5>
               <a href="badboy.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
@@ -94,8 +87,8 @@
       </div>
       <div class="col-12 col-md-4">
         <div class="card text-center mb-3" data-aos="fade-down">
-          <img src="Imagens/Card2.jpg" class="card-img-top" alt="Imagens/Badboy in a wasteland.png" id="image-card">
-          <div class="card-body bg-secondary">
+          <img src="Imagens/Card2.jpg" class="card-img-top" alt="Jogo2" id="image-card">
+          <div class="card-body">
             <h5 class="card-title text-uppercase">Jogo2</h5>
             <a href="jogo2.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
             <a href="jjogo2.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
@@ -104,8 +97,8 @@
       </div>
       <div class="col-12 col-md-4">
         <div class="card text-center mb-3" data-aos="fade-down-left">
-          <img src="Imagens/Card3.webp" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
-          <div class="card-body bg-secondary">
+          <img src="Imagens/Card3.webp" class="card-img-top" alt="Jogo3" id="image-card">
+          <div class="card-body">
             <h5 class="card-title text-uppercase">Jogo3</h5>
             <a href="jogo3.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
             <a href="jjogo.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
@@ -114,9 +107,8 @@
       </div>
       <div class="col-12 col-md-4">
         <div class="card text-center mb-3" data-aos="fade-up-right">
-          <img src="Imagens/Card4.avif" class="card-img-top" alt="Imagens/Badboy in a wasteland.png"
-            id="card-foto">
-          <div class="card-body bg-secondary">
+          <img src="Imagens/Card4.avif" class="card-img-top" alt="Jogo4" id="image-card">
+          <div class="card-body">
             <h5 class="card-title text-uppercase">Jogo4</h5>
             <a href="jogo4.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
             <a href="jjogo4.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
@@ -125,8 +117,8 @@
       </div>
       <div class="col-12 col-md-4">
         <div class="card text-center mb-3" data-aos="zoom-in">
-          <img src="Imagens/Card5.webp" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
-          <div class="card-body bg-secondary">
+          <img src="Imagens/Card5.webp" class="card-img-top" alt="Jogo5" id="image-card">
+          <div class="card-body">
             <h5 class="card-title text-uppercase">Jogo5</h5>
             <a href="jogo5.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
             <a href="jjogo5.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
@@ -135,17 +127,21 @@
       </div>
       <div class="col-12 col-md-4">
         <div class="card text-center mb-3" data-aos="fade-up-left">
-          <img src="Imagens/Card6.jpeg" class="card-img-top" alt="Imagens/Badboy in a wasteland.png">
-          <div class="card-body bg-secondary">
+          <img src="Imagens/Card6.jpeg" class="card-img-top" alt="Jogo6" id="image-card">
+          <div class="card-body">
             <h5 class="card-title text-uppercase">Jogo6</h5>
             <a href="jogo6.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
-            <a href="jjogo6.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>ogar</a>
+            <a href="jjogo6.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
           </div>
         </div>
       </div>
     </div>
   </div>
 </body>
+<footer>
+  <p class="text-center bg-secondary">Site desenvolvido por Emanuel Ramos Paiva</p>
+  </div>
+</footer>
 <script src="bootstrap-5.3.3/dist/js/bootstrap.bundle.js"></script>
 <script src="https://kit.fontawesome.com/4d675d3e85.js" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
