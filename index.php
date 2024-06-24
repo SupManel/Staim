26f7ceb43d3eff7a84cf2b7ee2f2de7ccddcb132
SupManel <emanuelramospaiva@gmail.com>
2024-06-24 16:01:51 -0300

    Atualizando um pouco de tudo e retirando algumas paginas

--- index.php
+++ index.php
@@ -28,19 +28,21 @@
             <a class="nav-link active" aria-current="page" href="index.php">Home</a>
           </li>
           <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="quemsomos.php">Quem Somos</a>
+          </li>
+          <li class="nav-item">
             <a class="nav-link active" href="faleconosco.php">Fale Conosco</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            <a class="nav-link dropdown-toggle active" href="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
               Games
             </a>
             <ul class="dropdown-menu">
               <li><a class="dropdown-item" href="badboy.php">BadBoy in a Dreamland</a></li>
-              <li><a class="dropdown-item" href="jogo2.php">Jogo2</a></li>
-              <li><a class="dropdown-item" href="jogo3.php">Jogo3</a></li>
-              <li><a class="dropdown-item" href="jogo4.php">Jogo4</a></li>
-              <li><a class="dropdown-item" href="jogo5.php">Jogo5</a></li>
-              <li><a class="dropdown-item" href="jogo6.php">Jogo6</a></li>
+              <li><a class="dropdown-item" href="jogo2.php">Rocket On The Rocks</a></li>
+              <li><a class="dropdown-item" href="jogo3.php">Tommy Adventure</a></li>
+              <li><a class="dropdown-item" href="jogo4.php">Curse Game</a></li>
             </ul>
           </li>
       </div>
@@ -49,16 +51,16 @@
   <div id="carouselExample" class="carousel slide">
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="Imagens/Destiny.webp" class="d-block w-100" alt="Jogo">
+        <img src="Imagens/BannerCowboy.jpg" class="d-block w-100" alt="Jogo">
       </div>
       <div class="carousel-item">
-        <img src="Imagens/Carro.jpg" class="d-block w-100" alt="Jogo">
+        <img src="Imagens/BannerAsteroide.jpg" class="d-block w-100" alt="Jogo">
       </div>
       <div class="carousel-item">
-        <img src="Imagens/Iki.webp" class="d-block w-100" alt="Jogo">
+        <img src="Imagens/BannerCurse.jpg" class="d-block w-100" alt="Jogo">
       </div>
       <div class="carousel-item">
-        <img src="Imagens/Logojogo2.jpg" class="d-block w-100" alt="Jogo">
+        <img src="Imagens/BannerTommy.jpg" class="d-block w-100" alt="Jogo">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -73,9 +75,9 @@
   <div class="container">
     <h1 class="text-center text-uppercase">Melhores Games</h1>
     <div class="row">
-      <div class="col-12 col-md-4">
-        <div class="card text-center mb-3" data-aos="fade-down-right">
-          <img src="Imagens/Badboy in a wasteland.png" class="card-img-top" alt="BadBoy in a Dreamland" id="image-card">
+      <div class="col-12 col-md-3">
+        <div class="card text-center mb-3" data-aos="fade-down">
+          <img src="Imagens/Cowboy.jpg" class="card-img-top" alt="BadBoy in a Dreamland" id="image-card">
           <div class="card-body">
             <div class="text-center">
               <h5 class="card-title text-uppercase">BadBoy in a Dreamland</h5>
@@ -85,53 +87,33 @@
           </div>
         </div>
       </div>
-      <div class="col-12 col-md-4">
+      <div class="col-12 col-md-3">
         <div class="card text-center mb-3" data-aos="fade-down">
-          <img src="Imagens/Card2.jpg" class="card-img-top" alt="Jogo2" id="image-card">
+          <img src="Imagens/Asteroide.jpg" class="card-img-top" alt="Rocket On The Rocks" id="image-card">
           <div class="card-body">
-            <h5 class="card-title text-uppercase">Jogo2</h5>
+            <h5 class="card-title text-uppercase">Rocket On The Rocks</h5>
             <a href="jogo2.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
             <a href="jjogo2.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
           </div>
         </div>
       </div>
-      <div class="col-12 col-md-4">
-        <div class="card text-center mb-3" data-aos="fade-down-left">
-          <img src="Imagens/Card3.webp" class="card-img-top" alt="Jogo3" id="image-card">
+      <div class="col-12 col-md-3">
+        <div class="card text-center mb-3" data-aos="fade-down">
+          <img src="Imagens/Tommy.jpg" class="card-img-top" alt="Tommy Adventure" id="image-card">
           <div class="card-body">
-            <h5 class="card-title text-uppercase">Jogo3</h5>
+            <h5 class="card-title text-uppercase">Tommy Adventure</h5>
             <a href="jogo3.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
             <a href="jjogo.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
           </div>
         </div>
       </div>
-      <div class="col-12 col-md-4">
-        <div class="card text-center mb-3" data-aos="fade-up-right">
-          <img src="Imagens/Card4.avif" class="card-img-top" alt="Jogo4" id="image-card">
+      <div class="col-12 col-md-3">
+        <div class="card text-center mb-3" data-aos="fade-down">
+          <img src="Imagens/Curse.jpg" class="card-img-top" alt="Curse Game" id="image-card">
           <div class="card-body">
-            <h5 class="card-title text-uppercase">Jogo4</h5>
+            <h5 class="card-title text-uppercase">Curse Game</h5>
             <a href="jogo4.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
             <a href="jjogo4.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-4">
-        <div class="card text-center mb-3" data-aos="zoom-in">
-          <img src="Imagens/Card5.webp" class="card-img-top" alt="Jogo5" id="image-card">
-          <div class="card-body">
-            <h5 class="card-title text-uppercase">Jogo5</h5>
-            <a href="jogo5.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
-            <a href="jjogo5.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
-          </div>
-        </div>
-      </div>
-      <div class="col-12 col-md-4">
-        <div class="card text-center mb-3" data-aos="fade-up-left">
-          <img src="Imagens/Card6.jpeg" class="card-img-top" alt="Jogo6" id="image-card">
-          <div class="card-body">
-            <h5 class="card-title text-uppercase">Jogo6</h5>
-            <a href="jogo6.php" class="btn btn-info text-uppercase"><i class="fa-solid fa-circle-info"></i>Info</a>
-            <a href="jjogo6.php" class="btn btn-success text-uppercase"><i class="fa-solid fa-play"></i>Jogar</a>
           </div>
         </div>
       </div>
@@ -139,8 +121,10 @@
   </div>
 </body>
 <footer>
-  <p class="text-center bg-secondary">Site desenvolvido por Emanuel Ramos Paiva</p>
-  </div>
+  <p class="text-center bg-secondary" id="footer">
+    <a href="https://github.com/SupManel">GitHub<i class="fa-brands fa-github"></i></a>
+    <a href="https://www.instagram.com/emanuel_r_p/">Instagram<i class="fa-brands fa-instagram"></i></a>
+  </p>
 </footer>
 <script src="bootstrap-5.3.3/dist/js/bootstrap.bundle.js"></script>
 <script src="https://kit.fontawesome.com/4d675d3e85.js" crossorigin="anonymous"></script>
